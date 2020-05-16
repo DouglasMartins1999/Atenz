4eaa9d79befbcaaa6e77834f226ce22389a17a51
Douglas Martins <douglas.martins.m099@gmail.com>
2020-05-15 23:54:15 -0300

    Login Screen Layout Implemented

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './content/profile/profile.component';
+import { LoginComponent } from './login/login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ProfileComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
