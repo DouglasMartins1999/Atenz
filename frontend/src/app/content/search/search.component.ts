53c47652bbf45b1a54bdccd1e5fde8e32067f0a4
Douglas Martins <douglas.martins.m099@gmail.com>
2020-05-31 23:06:49 -0300

    Rudimentar Search Implemented; Fixes at Lesson Component

--- frontend/src/app/content/search/search.component.ts
+++ frontend/src/app/content/search/search.component.ts
@@ -1,19 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, mergeMap, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'atz-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+	selector: 'atz-search',
+	templateUrl: './search.component.html',
+	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  query = "GraphQL"
-  featured = [false, true]
+	query: string;
+	content;
+	featured;
 
-  itens = [false, true, true, false]
+	itens = [false, true, true, false]
 
-  constructor() { }
+	constructor(
+		private router: Router,
+		private route: ActivatedRoute,
+		private http: HttpClient
+	) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.route.queryParams
+			.pipe(
+				filter(params => params.q),
+				mergeMap(params => {
+					this.query = params.q;
+					return this.search(params.q).pipe(map(item => { 
+						this.content = item;
+						return params;
+					}))
+				}),
+				mergeMap(params => {
+					return this.searchfeatured(params.q).pipe(map(item => {
+						this.featured = item;
+						return params;
+					}))
+				})
+			)
+			.subscribe();
+	}
 
+	search(data){
+		return this.http.get("/api/courses/search?q=" + data)
+	}
+
+	searchfeatured(data){
+		return this.http.get("/api/courses/search/featured?q=" + data)
+	}
+
+	goToItem(item){
+		const nav: any = {}
+		if(item.lesson && typeof item.lesson == "number") nav.lesson = item.lesson;
+		if(item.module && typeof item.module == "number") nav.module = item.module;
+		if(item.course && typeof item.course == "number") nav.course = item.course;
+		if(item.id && typeof item.id == "number") nav.lesson = item.id;
+
+		this.router.navigate(['view'], { queryParams: nav })
+	}
+
+	
 }
